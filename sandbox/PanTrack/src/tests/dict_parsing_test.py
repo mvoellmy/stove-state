731532df431b1c0d05a5988136531a2a6eac911d
mvoellmy <rudi.carfell@gmail.com>
2017-05-11 15:05:26 +0200

    updated pan_detector model/feature/info saving and cleaned up a lot of code added example files for the configs

--- sandbox/PanTrack/src/tests/dict_parsing_test.py
+++ sandbox/PanTrack/src/tests/dict_parsing_test.py
@@ -1,5 +1,6 @@
 # Dictionary Parsing Test
 # https://www.github.com/mvoellmy/
+import numpy as np
 
 write_dict = {}
 write_dict['string'] = 'string'
@@ -7,18 +8,21 @@ write_dict['int'] = 1
 write_dict['float'] = 3.2
 write_dict['tuple'] = (1, 2)
 write_dict['list'] = [1, 2, 5]
+write_dict['dict'] = {'tuple': (1, 2),
+                      'string': 'this is a string'}
 # ...add additional datatypes here to see if it works...
-# write_dict['data_type'] = data
+# These DO NOT WORK:
+# write_dict['np_array'] = np.zeros(3)
 
 print('Dictionary Parsing Test')
 
 # write dictionary to text file
-with open('dict_test.txt', 'w') as csvfile:
-    csvfile.write(repr(write_dict))
+with open('dict_test.txt', 'w') as file:
+    file.write(repr(write_dict))
 
 # read dictionary from text file
-with open('dict_test.txt', 'r') as csvfile:
-    read_dict = eval(csvfile.read())
+with open('dict_test.txt', 'r') as file:
+    read_dict = eval(file.read())
 
 # if the outputs of write and read dict are the same it worked
 print('#############')
@@ -30,3 +34,4 @@ print('#############')
 print('read\t{}'.format(type(read_dict)))
 for key, value in read_dict.items():
     print('{0}:\t{2}\t{1}'.format(key, value, type(value)))
+
--- sandbox/PanTrack/src/train_pan_detector.py
+++ sandbox/PanTrack/src/train_pan_detector.py
@@ -2,48 +2,49 @@ import cv2
 import configparser
 import ast
 import numpy as np
-import csv
 import os.path
 import time
 import pickle
 
 from random import shuffle
-
-from skimage import feature
+# Sk learn
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
-from scipy.stats import randint as sp_randint
-
-import matplotlib.pyplot as plt
 
 # Own Libraries
 from locate_pan import locate_pan
 from helpers import mse, get_HOG, histogram_equalization
 
 # Hog Params
-_params = {'orientations': 4,
-           'pixels_per_cell': (16, 16),
-           'cells_per_block': (4, 4),
-           'widthPadding': 10}
+_feature_params = {'orientations':      4,
+                   'pixels_per_cell':   (16, 16),
+                   'cells_per_block':   (4, 4),
+                   'widthPadding':      10}
 
-# Options
-_params['stove_type'] = 'I'
+# Features Info Parameters
+_params = {'stove_type':        'I',
+           'plate_of_interest': 4,
+           'feature_type':      'HOG',
+           'nr_of_features':    0,
+           'feature_params':    _feature_params}
+
+# Paths
 img_type = '.jpg'
 cfg_path = '../../../cfg/class_cfg.txt'
-features_path = '../features/'
-models_path = '../models/'
-features_name = '2017-04-27-15_17_27'
+features_name = '2017-05-11-14_52_03'
 
-_load_features = True
 _train_model = True
-_perc_jump = 10
+_load_features = _train_model
 _max_features = 5000
+_test_size = 0.3
 
+_use_mse = False
 _use_rgb = False
 _locate_pan = False
+
+# Output Options
+_print_update_rate = 100
 _plot_fails = True
 _plot_patches = False
 
@@ -52,20 +53,25 @@ config = configparser.ConfigParser()
 config.read(cfg_path)
 
 # Read corners and reshape them into 2d-Array
-corners = np.reshape(ast.literal_eval(config.get(_params['stove_type'], "corners")), (-1, 4))
-
-path_data = config.get('paths', 'data') + _params['stove_type'] + '/'
+_params['corners'] = ast.literal_eval(config.get(_params['stove_type'], "corners"))
+corners = np.reshape(_params['corners'], (-1, 4))
+polybox_path = config.get('paths', 'polybox')
 threshold = float(config.get(_params['stove_type'], "threshold"))
 plate_of_interest = int(config.get(_params['stove_type'], "plate_of_interest"))
 
+# More Paths
+features_path = polybox_path + 'pan_detect/features/'
+models_path = polybox_path + 'pan_detect/models/'
+data_path = polybox_path + 'pan_detect/data/' + _params['stove_type'] + '_' + str(_params['plate_of_interest']) + '/'
+
 # get classes
-class_list = [f for f in os.listdir(path_data) if os.path.isdir(os.path.join(path_data, f))]
+label_types = [f for f in os.listdir(data_path) if os.path.isdir(os.path.join(data_path, f))]
 
 # initialize variables
 labels = []
 data = []
 patches = []
-next_perc = 0
+print_update_state = _print_update_rate
 
 
 # Build or load Features
@@ -78,37 +84,43 @@ if _load_features:
     # load info file
     _params = {}
     print('Parameters: ')
-    for key, val in csv.reader(open(features_path + 'I_' + features_name + '.csv')):
-        _params[key] = val
-        print('     {}: {}'.format(key, val))
+
+    with open(features_path + 'I_' + features_name + '.txt', 'r') as file:
+        _params = eval(file.read())
+
+    for key, val in _params.items():
+        print('\t{}: {}'.format(key, val))
+
 else:
-    for label_nr, label_name in enumerate(class_list):
+    for label_nr, label_name in enumerate(label_types):
 
         nr_of_label_features = 0
 
-        img_list = [f for f in os.listdir(path_data+label_name) if os.path.isfile(os.path.join(path_data+label_name, f))
-                                                                   and img_type in f]
+        img_list = [f for f in os.listdir(data_path + label_name) if os.path.isfile(os.path.join(data_path + label_name, f))
+                    and img_type in f]
         print('{}:\nExtracting features from {} images...'.format(label_name, len(img_list)))
 
+        # Randomize img list so
         shuffle(img_list)
         for it, img in enumerate(img_list):
-            if nr_of_label_features >= _max_features/len(class_list):
+            if nr_of_label_features >= _max_features/len(label_types):
                 print("Max number of features for class {} has been reached".format(label_name))
                 break
 
-            frame = cv2.imread(path_data+label_name+'/'+img, 0)
+            frame = cv2.imread(data_path + label_name + '/' + img, 0)
             patch = frame[corners[plate_of_interest-1, 1]:corners[plate_of_interest-1, 3],
                           corners[plate_of_interest-1, 0]:corners[plate_of_interest-1, 2]]
 
             # Check the mean squared error between two consecutive frames
-            if it == 0 or mse(patch, old_patch) > threshold:
+            if it == 0 or not _use_mse or mse(patch, old_patch)\
+                    > threshold:
 
                 patch_normalized = histogram_equalization(patch)
                 hog = get_HOG(patch_normalized,
-                              orientations=_params['orientations'],
-                              pixels_per_cell=_params['pixels_per_cell'],
-                              cells_per_block=_params['cells_per_block'],
-                              widthPadding=_params['widthPadding'])
+                              orientations=_feature_params['orientations'],
+                              pixels_per_cell=_feature_params['pixels_per_cell'],
+                              cells_per_block=_feature_params['cells_per_block'],
+                              widthPadding=_feature_params['widthPadding'])
 
                 data.append(hog)
                 labels.append(label_nr)
@@ -127,38 +139,40 @@ else:
                 # cv2.imshow('frame', frame)
                 cv2.waitKey(1)
 
-            if it/len(img_list)*100 >= next_perc:
-                print("{:.2f} %".format(it/len(img_list)*100))
-                next_perc = next_perc + _perc_jump
+            if it >= print_update_state:
+                print("{}/{} features extracted".format(it, len(img_list)))
+                print_update_state = print_update_state + _print_update_rate
 
             old_patch = patch
 
         print('{} features extracted'.format(len(data)))
-        next_perc = 0
+        print_update_state = _print_update_rate
 
-    _params['labels'] = class_list
+    _params['labels'] = label_types
     _params['nr_features'] = len(data)
 
-    # Save features
-    f_time_name = time.strftime("%Y-%m-%d-%H_%M_%S")
-    features_name = features_path + 'F_' + f_time_name + '.npy'
-    labels_name = features_path + 'L_' + f_time_name + '.npy'
-    info_name = features_path + 'I_' + f_time_name + '.csv'
-
-    np.save(features_name, data)
-    np.save(labels_name, labels)
-    # save I in csv file
-    with open(info_name, 'w') as csvfile:
-        writer = csv.writer(csvfile)
-        for key, val in _params.items():
-            writer.writerow([key, val])
-
     print('Feature extraction finished!')
+    print('---------------------------')
+
+    # Save features
+    if input('Save features? [y/n]').lower() == 'y':
+        f_time_name = time.strftime("%Y-%m-%d-%H_%M_%S")
+        features_name = features_path + 'F_' + f_time_name + '.npy'
+        labels_name = features_path + 'L_' + f_time_name + '.npy'
+        info_name = features_path + 'I_' + f_time_name + '.txt'
+
+        np.save(features_name, data)
+        np.save(labels_name, labels)
+        # save I=Info in txt file
+        with open(info_name, 'w') as file:
+            file.write(repr(_params))
+
+        print("Features have been saved.")
+
 
 print('---------------------------')
-
 if _train_model:
-    train_data, test_data, train_labels, test_labels = train_test_split(data, labels, test_size=0.2, random_state=2)
+    train_data, test_data, train_labels, test_labels = train_test_split(data, labels, test_size=_test_size, random_state=2)
     # Optimize the parameters by cross-validation
     parameters = [
         # {'kernel': ['rbf'], 'gamma': [0.1, 1], 'C': [1, 100]},
@@ -177,26 +191,29 @@ if _train_model:
 
     print("Starting test dataset...")
     labels_predicted = clf.predict(test_data)
-    _params['model_accuracy'] = (labels_predicted == test_labels).mean()
-    print("Test Accuracy [%0.3f]" % (_params['model_accuracy']))
+    _model_params = {'model_accuracy': (labels_predicted == test_labels).mean(),
+                     'model_params': clf.best_params_,
+                     'test_size': _test_size}
 
-    # save the model to disk
-    m_time_name = time.strftime("%Y-%m-%d-%H_%M_%S")
-    model_name = models_path + 'M_' + m_time_name + '.sav'
-    info_name = models_path + 'I_' + m_time_name + '.csv'
+    _params['model_params'] = _model_params
 
-    pickle.dump(clf, open(model_name, 'wb'))
-    with open(info_name, 'w') as csvfile:
-        writer = csv.writer(csvfile)
-        for key, val in _params.items():
-            writer.writerow([key, val])
+    print("Test Accuracy {}".format(_model_params['model_accuracy']))
 
+    if input('Save model? [y/n]').lower() == 'y':
+        # save the model to disk
+        m_time_name = time.strftime("%Y-%m-%d-%H_%M_%S")
+        model_name = models_path + 'M_' + m_time_name + '.sav'
+        info_name = models_path + 'I_' + m_time_name + '.txt'
 
-    print("Model has been saved.")
+        pickle.dump(clf, open(model_name, 'wb'))
 
+        with open(info_name, 'w') as file:
+            file.write(repr(_params))
+
+        print("Model has been saved.")
 
     if _plot_fails:
         for i, image in enumerate(test_data):
-           if labels_predicted[i] != test_labels[i]:
-               cv2.imshow('{} Predicted: {} Truth: {}'.format(i, labels_predicted[i], test_labels[i]), patches[i])
-               cv2.waitKey(0)
+            if labels_predicted[i] != test_labels[i]:
+                cv2.imshow('{} Predicted: {} Truth: {}'.format(i, labels_predicted[i], test_labels[i]), patches[i])
+                cv2.waitKey(0)
